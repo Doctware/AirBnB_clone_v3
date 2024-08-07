44eef43779c391021d376e6288d0c0bfc3c50246
Doctware <yusclever9@gmail.com>
2024-08-07 11:40:34 +0100

    updated

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ creating blue print for easy routing """
 from flask import Blueprint
-
+from api.v1.views.index import *
+from api.vi.views.state import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
