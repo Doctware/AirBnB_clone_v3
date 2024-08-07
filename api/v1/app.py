8552d2ec3451ef05e31599eb36853829d115d1fb
Doctware <yusclever9@gmail.com>
2024-08-07 02:56:19 +0100

    Error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ the module  app """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """ this method access the storage then closed aafter opperatins """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ this method response to a not found page """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
