0b8d62fb944216ef0c41c8dc99e18c61d2fb9d31
Doctware <yusclever9@gmail.com>
2024-08-08 15:40:50 +0100

    State RESTfull fixed

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,28 +3,40 @@
 from models.state import State
 from flask import jsonify, request, abort
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """ this mehods retrives list of all state obj """
     states = storage.all(State).values()
-    return jsonify([state.to_dict() for state in states])
+    return jsonify({"states": [state.to_dict() for state in states]})
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
-    """ retrives state obj """
-    state = storge.get(State, state_id)
+    """ retrives state obj base on provided id """
+    states = storage.all(State)
+    state = None
+    for obj in states.values():
+        if obj.id == state_id:
+            state = obj
+            break
     if not state:
         abort(404)
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
-    """ this method delete provided stae oobj"""
-    state = storage.get(State, state_id)
+    """ this method delete obj base on provided id"""
+    states = storage.all(State)
+    state = None
+    for obj in states.values():
+        if obj.id == state_id:
+            state = obj
+            break
     if not state:
         abort(404)
     storage.delete(state)
@@ -49,10 +61,15 @@ def create_state():
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """ update the given obj from State """
-    state == storage.get(State, state_id)
+    states = storage.all(State)
+    state = None
+    for obj in states.values():
+        if obj.id == state_id:
+            state = obj
+            break
     if not state:
         abort(404)
-    if not request.json():
+    if not request.get_json():
         abort(404, discription="Not a JSON")
     data = request.get_json()
     ignore_keys = ['id', 'create_id', 'updated_at']
