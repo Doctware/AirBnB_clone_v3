737edb209ac4a41ec15f0366f76d47a4ed2c9e5b
Doctware <yusclever9@gmail.com>
2024-08-09 23:03:43 +0100

    Under writing

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -11,3 +11,36 @@ def get_amenities():
     """ this method retrieves all amenities object """
     amenities = storage.all(Amenity)
     return jsonify([amenity.to_dict()for amenity in amenities.values()])
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def get_amenity(amenity_id):
+    """ this method retrieves amenities obj base on amenity id provided """
+    amenities = storage.all(Amenity).get("amenity." + amenity_id)
+    if not amenity:
+        abort(404)
+    return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def del_amenity(amenity_id):
+    """ this method delete Amenity obj base on given id """
+    amenities = storage.all(Amenity).get("amenity." + amenity_id)
+    if not amenity:
+        abort(404)
+    storage.delete(amenity)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/amenities', metthods=['POST'])
+def create_amenity():
+    """ this method create obj on Amenity """
+    if not request.is_json:
+        abort(400, discriptoin="Not a JSON")
+    data = request.get_json()
+    if 'name' not in data:
+        abort(400, discription="Missing name")
+    new_amenity = Amenity(**data)
+    new_amenity.save()
+    return jsonify(new_amenity.to_dict())
